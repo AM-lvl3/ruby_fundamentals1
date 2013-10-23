adab9220d44f711c873433a3e21b327cf75c3d06
AM-lvl3 <drewmorris@live.com>
2013-10-23 09:28:11 -0400

    Now includes age

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,8 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you #{name}?"
+age = gets.chomp
+born = 2013-age.to_i
+puts "Really? So you were born in #{born}!"
